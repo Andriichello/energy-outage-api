d1180d5591b043ff7eec0f4f0cf5da100c29dba3
Andrii Prykhodko <andriichello@gmail.com>
2026-01-21 11:41:10 +0200

    Update throttle on fetch endpoint to be 1 request per 1 minute

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('updated-information/fetch', [App\Http\Controllers\UpdatedInformationController::class, 'fetch'])
-    ->middleware('throttle:1,5');
+Route::get('/updated-information/fetch', [App\Http\Controllers\UpdatedInformationController::class, 'fetch'])
+    ->middleware('throttle:1,1');
 
 Route::post('/telegram/{token}/webhook', WebhookController::class)
     ->withoutMiddleware('throttle');
